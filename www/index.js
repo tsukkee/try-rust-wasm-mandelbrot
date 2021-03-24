37aedcc31e73d9355732548a4b3aa91e3c4ac88d
tsukkee <takayuki0510@gmail.com>
2021-03-24 17:21:19 +0900

    add www directory

---
+++ www/index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const DEFAULT_MAX_ITER = 64;
+const mandelbrot = import("../pkg").catch(console.error);
+
+Promise.all([mandelbrot]).then(async function([
+    { draw_mandelbrot_set }
+]) {
+  const canvas = document.getElementById("canvas_wasm");
+  const maxIterRange = document.getElementById("max_iter_range");
+  const maxIterDisplay = document.getElementById("max_iter_display");
+
+  maxIterRange.value = DEFAULT_MAX_ITER;
+  maxIterDisplay.textContent = DEFAULT_MAX_ITER;
+  draw_mandelbrot_set(canvas, DEFAULT_MAX_ITER);
+  maxIterRange.addEventListener("input", () => {
+    const maxIter = maxIterRange.value;
+    maxIterDisplay.textContent = maxIter;
+    draw_mandelbrot_set(canvas, maxIter);
+  });
+});
+
